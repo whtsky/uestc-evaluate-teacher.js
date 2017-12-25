7b35df5d773621e640bccc5e615ee0a1dc514107
Wu Haotian <whtsky@gmail.com>
2017-12-25 21:39:44 +0800

    fix 榴莲吃掉的空格

--- evaluate.js
+++ evaluate.js
@@ -1,6 +1,8 @@
 // 我热爱我的老师们，所以我要给他们全评满分
 // 打开评教页面，运行，等全部评教完成之后手动点一下提交
-window.confirm = () = > true
+
+window.confirm = () => true
+
 function teacherGood() {
   document.getElementById('op_6_15').checked = true;
   document.getElementById('op_2_25').checked = true;
@@ -9,12 +11,14 @@ function teacherGood() {
   bg.form.submit('evaluateEditForm', null, null, 'doPost();');
   setTimeout(clickNext, 2000)
 }
+
 function clickNext() {
-  lettag = Array.from(document.getElementsByTagName('a')).find(tag = > tag.innerHTML.includes('进行评教'))
+  let tag = Array.from(document.getElementsByTagName('a')).find(tag => tag.innerHTML.includes('进行评教'))
   if (!tag) {
     return
   }
   tag.click()
   setTimeout(teacherGood, 2000)
 }
+
 clickNext()
