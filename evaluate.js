2d9966950a1e01c358db58c10cfad26fe884ebca
黄ZY <huangziyi010@live.cn>
2018-12-21 14:50:27 +0800

    not done

--- evaluate.js
+++ evaluate.js
@@ -3,6 +3,34 @@
 
 window.confirm = () => true
 
+function genRandom(m,n){
+  let ans = Array(n);
+  
+}
+
+//打星星
+function starCourse() {
+  //读取优秀课程的数量
+  let cdiv = document.getElementById('contentDiv');
+  let nums = /优秀课程.*?(\d).*?(\d)/.exec(cdiv.getElementsByTagName('label')[0].textContent);
+  //取平均
+  let num = Math.floor((nums[1]*1+nums[2]*1)/2);
+
+  //读取表格
+  let eform = document.getElementById('evaluateForm');
+  let einput = eform.getElementsByTagName('input');
+
+  //随机5星
+
+
+
+
+  for(i=0;i<einput.length;i++){
+    if(einput[i].id!==null)
+      document.getElementById(einput[i].id).value=5;
+  }
+}
+
 function teacherGood() {
   document.getElementById('op_6_15').checked = true;
   document.getElementById('op_2_25').checked = true;
